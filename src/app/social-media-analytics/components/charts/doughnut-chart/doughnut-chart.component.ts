6142f2c6c0278fb6fb6960be421e6ef2dadc5e6d
Ishara Kudagamage <141898107+IKudagama@users.noreply.github.com>
2024-07-03 15:38:39 +0530

    GaugeChart _ Instagram added.

--- src/app/social-media-analytics/components/charts/doughnut-chart/doughnut-chart.component.ts
+++ src/app/social-media-analytics/components/charts/doughnut-chart/doughnut-chart.component.ts
@@ -42,9 +42,9 @@ export class DoughnutChartComponent implements OnInit {
                   '#5dd28d'
                 ],
                 hoverBackgroundColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)'
+                  '#f87171',
+                  '#facc15',
+                  '#4ade80'
                 ]
               }
             ]
