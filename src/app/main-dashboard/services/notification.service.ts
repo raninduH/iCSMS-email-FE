29915c7c3e651c7e0dedf3f82a87c7eb92c83219
unknown <uthayakumarthenujan@gmail.com>
2024-07-05 13:50:41 +0530

    notification service ip chnaged local to aww

--- src/app/main-dashboard/services/notification.service.ts
+++ src/app/main-dashboard/services/notification.service.ts
@@ -12,8 +12,8 @@ export class NotificationService {
   private messagesSubject$ = new Subject<any>();
   public messages$ = this.messagesSubject$.asObservable();
 
-  // private baseUrl = 'http://13.233.199.31:8001/Notifications';
-  private baseUrl = 'http://127.0.0.1:8001/Notifications';
+  private baseUrl = 'http://13.233.199.31:8001/Notifications';
+  // private baseUrl = 'http://127.0.0.1:8001/Notifications';
 
   constructor(private http: HttpClient) {
     this.connect();
