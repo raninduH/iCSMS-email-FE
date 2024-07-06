692c9b53781ced63d00e418cc8858538efaa6954
Wilbax <janithravisankax@gmail.com>
2024-07-07 00:54:27 +0530

    auth api ip changed

--- src/app/app-settings/config.ts
+++ src/app/app-settings/config.ts
@@ -2,7 +2,7 @@
 
 // export const apiEndpoint = 'http://localhost:8000';
 
-export const apiEndpoint = 'http://13.233.51.70:8000';
+export const apiEndpoint = 'http://65.0.110.120:8000';
 
 export const socialMediaBackendAPI = 'https://icsms-social-media-backend.vercel.app'
 //  export const socialMediaBackendAPI = 'http://localhost:8000'
